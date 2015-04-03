20da577352965f38c7035eaa41479ee0ae75174f
liaoliao <312998164@qq.com>
2015-04-03 18:16:06 +0800

    some update

---
+++ ogldev-qt/main.cpp
@@ -0,0 +1,21 @@
+#include "trianglewindow.h"
+
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+    //app.setApplicationName(" qt_ogldev");
+    //app.setApplicationVersion("0.1");
+    QSurfaceFormat format;
+    format.setSamples(16);
+
+    TriangleWindow window;
+    window.setFormat(format);
+    window.resize(1024,768);
+    window.show();
+    window.setAnimating(true);
+
+    return app.exec();
+}
+
+
