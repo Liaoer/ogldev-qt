d2579ca379ed8a8b3d59958e6ff684ace9deefa1
Liaoer <312998164@qq.com>
2015-06-26 00:55:20 +0800

    update

---
+++ ogldev-qt/base/boneinfo.h
@@ -0,0 +1,13 @@
+#include BONEINFO_H
+#include BONEINFO_H
+
+#include "ogldev_math_3d.h"
+class BoneInfo
+{
+public:
+	Matrix4f BoneOffset;
+	Matrix4f FinalTransformation;
+	BoneInfo();
+};
+
+#endif //BONEINFO_H
---
+++ ogldev-qt/base/node.h
@@ -0,0 +1,100 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <QMatrix4x4>
+#include <vector>
+#include <string>
+#include <functional>
+ 
+#define NODE_TYPE_NODE 0
+#define NODE_TYPE_ENTITY 1
+#define NODE_TYPE_CAMERA 2
+#define NODE_TYPE_SPRITE 4
+#define NODE_TYPE_CUSTOM 5
+ 
+
+enum class NodeGroup
+{
+	G_Default = 1,
+    G_1 = 1 << 1,
+    G_2 = 1 << 2,
+    G_3 = 1 << 3,
+    G_4 = 1 << 4,
+    G_5 = 1 << 5,
+    G_6 = 1 << 6,
+    G_7 = 1 << 7,
+    G_8 = 1 << 8,
+};
+
+class Scene;
+class Node : public Removable
+{
+public:
+	Node();
+	void addChild(Node *child);
+	void removeFromParent();
+	void removeChild(Node *child);
+	Node *parent() const;
+	void setParent(Node *parent);
+	std::vector<Node *> children() const;
+	void setChildren(const std::vector<Node *> &children);
+	void visit(Scene *scene);
+	int nodeType() const;
+	void setNodeType(int nodeType);
+	
+	void translate(float x, float y, float z);
+	void rotate(float x, float y, float z);
+	void scale(float x, float y, float z);
+	
+	QMatrix4x4 getModelTrans();
+    QMatrix4x4 getParentModelTrans();
+    QMatrix4x4 getRotaionMatrix();
+    QMatrix4x4 getScallingMatrix();
+    QMatrix4x4 getTranslationMatrix();
+    const char *name() ;
+    void setName(const char *name);
+	
+	void setYaw(float yaw);
+    float getYaw();
+
+    void setPitch(float pitch);
+    float getPitch();
+
+    void setRoll(float roll);
+    float getRoll();
+	
+	void yawBy(float value);
+    void pitchBy(float value);
+    void rollBy(float value);
+    void moveBy(float the_x, float the_y, float the_z,bool isRelative = true);
+    void move(float the_x, float the_y, float the_z);
+	
+	QVector3D pos() const;
+    void setPos(const QVector3D &pos);
+
+	QVector3D rotation() const;
+    void setRotation(const QVector3D &rotation);
+	
+	QVector3D scalling() const;
+    void setScalling(const QVector3D &scalling);
+	
+	void setGroupMask(NodeGroup mask);
+
+    bool isGroupMask(NodeGroup mask);
+
+    QVector3D getForwardVector();
+public:
+	std::function<void (Node *)> onUpdate;
+protected:
+	 void setAsCustomNode();
+protected:
+	QVector3D m_pos;
+    QVector3D m_rotation;
+    QVector3D m_scalling;
+    int m_nodeType;
+    Node * m_parent;
+    std::vector<Node *>m_children;
+    std::string m_name;
+    unsigned int m_groupMask;
+}
+#endif // NODE_H
